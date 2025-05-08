e70e94dff0f2696342e57683b4e636c9e94c9c6d
Joolee Kalaria <joolee.kalaria@brainvire.com>
2025-05-08 12:45:31 +0530

    home page desgin fixes

--- resources/views/admin/lessons/card.blade.php
+++ resources/views/admin/lessons/card.blade.php
@@ -51,7 +51,7 @@
         <div class="mt-auto bg-gray-200 gap-1 rounded-lg px-4 py-3 flex">
             <div class="text-center w-50">
                 <span class="text-xl font-bold">{!! $model->lesson_quantity !!}</span>
-                <div class="text-sm rtl:space-x-reverse">Number of Lessons</div>
+                <div class="text-sm rtl:space-x-reverse">Number of <br> Lessons</div>
 
             </div>
             <div class="text-center w-50">
