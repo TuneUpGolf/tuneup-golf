58d248e2746bc0a26db909db32209eae43370b93
Nikhil <nikhil.sompura@brainvire.com>
2025-06-20 11:51:29 +0530

    Instructor lesson only query updated

--- app/Http/Controllers/Admin/LessonController.php
+++ app/Http/Controllers/Admin/LessonController.php
@@ -186,7 +186,7 @@ class LessonController extends Controller
             if($lesson) {
                  return view('admin.lessons.addSlot', compact('lesson'));
             } else {
-                $lesson = Lesson::withMax('packages','number_of_slot')->whereIn('type',['package','inPerson'])->get()->toArray();
+                $lesson = Lesson::withMax('packages','number_of_slot')->whereIn('type',['package','inPerson'])->where('created_by',Auth::user()->id)->get()->toArray();
                 return view('admin.lessons.setAvailability', compact('lesson'));
             }
            
