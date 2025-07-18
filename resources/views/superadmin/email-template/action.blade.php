ced4e9bcae4f509cbc7449b0258478b0358c5fba
Rajansinh Chudasama <rajan.chudasama@brainvire.com>
2025-07-18 12:26:18 +0530

    #521712: Resolved button issues in responsive for super admin

--- resources/views/superadmin/email-template/action.blade.php
+++ resources/views/superadmin/email-template/action.blade.php
@@ -1,6 +1,8 @@
 @can('edit-email-template')
-    <a class="btn btn-sm small btn-warning" href="{{ route('email-template.edit', $row->id) }}" data-bs-toggle="tooltip"
+<div class="action-btn-fix-wraper">
+    <a class="btn btn-sm small btn-warning action-btn-fix" href="{{ route('email-template.edit', $row->id) }}" data-bs-toggle="tooltip"
         data-bs-placement="bottom" data-bs-original-title="{{ __('Edit') }}">
         <i class="text-white ti ti-edit"></i>
     </a>
+</div>
 @endcan
--- resources/views/superadmin/users/action.blade.php
+++ resources/views/superadmin/users/action.blade.php
@@ -1,3 +1,4 @@
+<div class="action-btn-fix-wraper">
 {{-- @can('impersonate-user')
     <a class="btn btn-sm small btn btn-info" target="_new" href="{{ route('users.impersonate', $user->id) }}"
         data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="{{ __('Impersonate') }}">
@@ -5,7 +6,7 @@
     </a>
 @endcan --}}
 @can('edit-user')
-    <a class="btn btn-sm small btn btn-warning" href="{{ route('users.edit', $user->id) }}" data-bs-toggle="tooltip"
+    <a class="btn btn-sm small btn btn-warning action-btn-fix" href="{{ route('users.edit', $user->id) }}" data-bs-toggle="tooltip"
         data-bs-placement="bottom" data-bs-original-title="{{ __('Edit') }}">
         <i class="ti ti-edit text-white"></i>
     </a>
@@ -13,13 +14,14 @@
 @can('delete-user')
     {!! Form::open([
         'method' => 'DELETE',
-        'class' => 'd-inline',
+        'class' => 'd-flex',
         'route' => ['users.destroy', $user->id],
         'id' => 'delete-form-' . $user->id,
     ]) !!}
-    <a href="javascript:void(0);" class="btn btn-sm small btn btn-danger show_confirm" data-bs-toggle="tooltip"
+    <a href="javascript:void(0);" class="btn btn-sm small btn btn-danger show_confirm action-btn-fix" data-bs-toggle="tooltip"
         data-bs-placement="bottom" id="delete-form-1" data-bs-original-title="{{ __('Delete') }}">
         <i class="ti ti-trash text-white"></i>
     </a>
     {!! Form::close() !!}
 @endcan
+</div>
