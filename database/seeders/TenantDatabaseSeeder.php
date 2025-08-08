a274ac74538af1b3168ec4c1a7cf2ab1de39fd66
Rajansinh Chudasama <rajan.chudasama@brainvire.com>
2025-08-08 19:10:58 +0530

    #532237: Added new setting fields

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -310,6 +310,12 @@ class TenantDatabaseSeeder extends Seeder
 
             ['key' => 'footer_setting_enable', 'value' => 'off'],
             ['key' => 'footer_description', 'value' => 'Analyzing Golf'],
+            ['key' => 'social_url_fb', 'value' => null],
+            ['key' => 'social_url_x', 'value' => null],
+            ['key' => 'social_url_ig', 'value' => null],
+            ['key' => 'social_url_yt', 'value' => null],
+            ['key' => 'social_url_ln', 'value' => null],
+            ['key' => 'banner_image', 'value' => null],
         ];
 
         tenancy()->central(function ($tenant) {
