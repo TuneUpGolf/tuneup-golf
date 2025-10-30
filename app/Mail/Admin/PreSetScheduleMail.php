cea4ebbe8dc9a97a215c5f9786e12ff5ac8c67e5
rehmatweb <rehmatmughalofficialmail@gmail.com>
2025-10-30 15:02:49 +0500

    point 3 and point 4

---
+++ app/Mail/Admin/PreSetScheduleMail.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Mail\Admin;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\Base\InstructorTemplateMailable;
+
+class PreSetScheduleMail extends InstructorTemplateMailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $name;
+    public $date;
+    public $time;
+    public $notes;
+    public $description;
+
+    public function __construct($name,$date,$time,$notes=null,$description=null )
+    {
+        $this->name = $name;
+        $this->date = $date;
+        $this->time = $time;
+        $this->notes = $notes;
+        $this->description = $description;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function build()
+    {
+        return $this->html($this->buildView()); // Ensure HTML is sent
+    }
+  
+    
+    public function getHtmlLayout(): string
+    {
+        return view('mails.layout')->render();
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    // public function content()
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    // public function attachments()
+    // {
+    //     return [];
+    // }
+}
